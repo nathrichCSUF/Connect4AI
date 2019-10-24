40a765a315f2685fca788a805db0df0797593c09
MatthewCamarena <camarena.matthew@csu.fullerton.edu>
2019-10-24 16:50:45 -0700

    Added Quit event and fix for crashing

--- main.py
+++ main.py
@@ -15,23 +15,30 @@ import pygame
 from board import Board
 
 
-BLUE = (50, 94, 168)
-
-
 class Game:
+    BLACK = (50, 94, 168)
 
     def __init__(self):
         pygame.init()  # initialize pygame
         self.screen = pygame.display.set_mode((680, 740))  # screen size
         self.game_board = Board(self.screen)
+        
 
     def play(self):
-        while True:
 
+        intro = True
+        while intro:
             self.update_screen()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
 
     def update_screen(self):
-        self.screen.fill(BLUE)
+        self.screen.fill(game.BLACK)
+        self.game_board.blit()
+
+        pygame.display.flip()
 
 
 game = Game()
