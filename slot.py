f78d28f94e2afd211d6e5b35abbebabd42e3ce08
MatthewCamarena <camarena.matthew@csu.fullerton.edu>
2019-10-30 22:10:23 -0700

    Update
    Added functionality, so you can play the game the slider, the board, the coins. Unfortunately, the game will continue to play indefinitely so we will need a menu and make a reset function. As well as finally implementing the ai.

--- slot.py
+++ slot.py
@@ -18,16 +18,26 @@ class Slot:
     def __init__(self, screen):
         self.screen = screen
         self.state = "black"  # String holding state of coin, is it red, yellow, or black(empty)
-        self.image = pygame.image.load_extended("black.png")
+        self.image = pygame.image.load("black.png")
         self.rect = self.image.get_rect()
 
     def change_state(self, color):  # Change color of coin
         self.state = color
-        self.image = pygame.image.load_extended("images/" + self.state + ".png")
+        self.image = pygame.image.load(self.state + ".png")
 
     def set_slot_position(self, x, y):  # Set position of slot within the grid
         self.rect.x = x
         self.rect.y = y
+        #print("slot position x: " + str(x) + " y: " + str(y))
 
-    def blit(self):  # Blit Coin to Screen
+    def get_slot_position_x(self):
+        return self.rect.x
+
+    def get_slot_position_y(self):
+        return self.rect.y
+
+    # Blit Coin to Screen
+    def blit(self):  
+        #print(str(self.rect) + " < - blit coin")
         self.screen.blit(self.image, self.rect)
+
