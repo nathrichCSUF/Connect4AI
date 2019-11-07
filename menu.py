606cf6e31f744ba7cd41b768c2d546b1efdfda9e
Yash <yash_b@csu.fullerton.edu>
2019-11-06 21:23:39 -0800

    sprites update 2

--- menu.py
+++ menu.py
@@ -17,22 +17,23 @@ class Menu:
     def __init__(self,screen):
         self.screen = screen
         self.screen_rect = screen.get_rect()
-        self.title = pygame.image.load_extended("images/connect4_logo.png")
+        # self.title = pygame.image.load_extended("images/connect4_logo.png")
         self.playButton = pygame.image.load_extended("images/play_button.png")
         self.playButton = pygame.transform.scale(self.playButton, (96, 92))
         self.background = pygame.image.load_extended("images/bg-c4.png")
+        self.background = pygame.transform.scale(self.background, (696, 700))
 
-        self.titlerect = self.title.get_rect()
+        # self.titlerect = self.title.get_rect()
         self.playrect = self.playButton.get_rect()
 
-        self.titlerect.center = self.screen_rect.center
-        self.titlerect.top = self.screen_rect.top
+        # self.titlerect.center = self.screen_rect.center
+        # self.titlerect.top = self.screen_rect.top
 
         self.playrect.center = self.screen_rect.center
 
     def draw_menu(self):
         self.screen.blit(self.background, (0,0))
-        self.screen.blit(self.title, self.titlerect)
+        # self.screen.blit(self.title, self.titlerect)
         self.screen.blit(self.playButton, self.playrect)
 
     def checkForPlayButtonClick(self,mouseX,mouseY, settings):
