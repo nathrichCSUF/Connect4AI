85175a48391882d4c0ad26923d3bfebc93da3448
Yash Bhambhani <bhambhani.yash@gmail.com>
2019-11-06 22:21:41 -0800

    sprite update 4

--- menu.py
+++ menu.py
@@ -17,23 +17,25 @@ class Menu:
     def __init__(self,screen):
         self.screen = screen
         self.screen_rect = screen.get_rect()
-        self.title = pygame.image.load_extended("images/connect4_logo.png")
-        self.playButton = pygame.image.load_extended("images/play_button.png")
-        self.playButton = pygame.transform.scale(self.playButton, (96, 92))
+        # self.title = pygame.image.load_extended("images/connect4_logo.png")
+        self.pvpButton = pygame.image.load_extended("images/pvp.png")
+        self.pvpButton = pygame.transform.scale(self.pvpButton, (460, 115))
+        self.pvaiButton = pygame.image.load("images/pvai.png")
+        self.pvaiButton = pygame.transform.scale(self.pvaiButton, (460, 115))
         self.background = pygame.image.load_extended("images/bg-c4.png")
 
-        self.titlerect = self.title.get_rect()
-        self.playrect = self.playButton.get_rect()
-
-        self.titlerect.center = self.screen_rect.center
-        self.titlerect.top = self.screen_rect.top
-
-        self.playrect.center = self.screen_rect.center
+        # self.titlerect = self.title.get_rect()
+        self.pvprect = self.pvpButton.get_rect()
+        self.pvairect = self.pvaiButton.get_rect()
+        # self.titlerect.center = self.screen_rect.center
+        # self.titlerect.top = self.screen_rect.top
 
     def draw_menu(self):
         self.screen.blit(self.background, (0,0))
-        self.screen.blit(self.title, self.titlerect)
-        self.screen.blit(self.playButton, self.playrect)
+        # self.screen.blit(self.title, self.titlerect)
+        self.screen.blit(self.pvpButton, (20,300))
+        self.screen.blit(self.pvaiButton, (20,450))
+
 
     def checkForPlayButtonClick(self,mouseX,mouseY, settings):
         button_clicked = self.playrect.collidepoint(mouseX, mouseY) # check if mouse button clicked play button
